841fe8338215afd0f0ea95d8b58ea09c96113697
SAN.D <das95sankaram@gmail.com>
2023-12-13 01:29:14 +0530

    initial

--- src/app/_components/Footer.js
+++ src/app/_components/Footer.js
@@ -1,5 +1,10 @@
-const Footer = () => {
-    return <div>Hello from Footer</div>
+const Footer = ({data}) => {
+    const { text } = data;
+  return (
+    <div className="flex w-full flex-col mt-4 items-center justify-center text-center pt-10 mt-5 mb-10">
+      {text}
+    </div>
+  );
 }
 
 export default Footer;
--- src/app/page.js
+++ src/app/page.js
@@ -6,16 +6,33 @@ import Faq from "./_components/Faq"
 import Services from "./_components/Services"
 import Contact from "./_components/Contact"
 
-export default function Home() {
+const getLandingPageData = async () => {
+  const version = process.env.SB_DATA_VERSION;
+  const token = process.env.SB_TOKEN;
+  const url = `https://api.storyblok.com/v2/cdn/stories/landing-page?version=${version}&token=${token}`;
+  let req = await fetch(url, {cach: 'no-store'});
+
+  const storyData = await req.json();
+  const { nav_section, hero_section } = storyData.story.content;
+
+  return{
+    nav_section: nav_section[0],
+    hero_section: hero_section[0],
+  }
+}
+
+export default async function Home() {
+  const storyData = await getLandingPageData();
+
   return (
     <>
-    <Navbar />
-    <Hero />
-    <Services />
+    <Navbar data={storyData.nav_section} />
+    <Hero data={storyData.hero_section} />
+    {/* <Services />
     <Testimonial />
     <Contact />
     <Faq />
-    <Footer />
+    <Footer /> */}
     </>
   )
 }
